e646fc42ab1713106293a35e5dae3190eab983a0
dell <sujithsujithrejitha@gmail.com>
2023-10-26 18:09:46 +0530

    Initial commit

---
+++ bin/sujith4.dart
@@ -0,0 +1,5 @@
+import 'package:sujith4/sujith4.dart' as sujith4;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sujith4.calculate()}!');
+}
---
+++ test/sujith4_test.dart
@@ -0,0 +1,8 @@
+import 'package:sujith4/sujith4.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
